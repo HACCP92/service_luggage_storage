9041c6c23fb466dbf979dce2027b1a84ac07fe65
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-09-16 00:08:11 +0900

    [물품보관소 서비스 프로젝트] home_state 작성

---
+++ lib/presentation/home/home_state.dart
@@ -0,0 +1,56 @@
+import 'address.dart';
+
+class HomeState {
+  final String username;
+  final String? depart;
+  final String? arrive;
+  final List<Address> recentlyAddresses;
+  final double? latitude;
+  final double? longitude;
+  final String? imageUrl;
+  final int? smallSizeCount;
+  final int? mediumSizeCount;
+  final int? largeSizeCount;
+
+  HomeState({
+    required this.username,
+    this.depart,
+    this.arrive,
+    this.recentlyAddresses = const [],
+    this.latitude,
+    this.longitude,
+    this.imageUrl,
+    this.smallSizeCount,
+    this.mediumSizeCount,
+    this.largeSizeCount,
+  });
+
+  HomeState copyWith({
+    String? username,
+    String? depart,
+    String? arrive,
+    List<Address>? recentlyAddresses,
+    double? latitude,
+    double? longitude,
+    String? imageUrl,
+    int? smallSizeCount,
+    int? mediumSizeCount,
+    int? largeSizeCount,
+    required (double, double) currentLocation,
+  }) {
+    return HomeState(
+      username: username ?? this.username,
+      depart: depart ?? this.depart,
+      arrive: arrive ?? this.arrive,
+      recentlyAddresses: recentlyAddresses ?? this.recentlyAddresses,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+      imageUrl: imageUrl ?? this.imageUrl,
+      smallSizeCount: smallSizeCount ?? this.smallSizeCount,
+      mediumSizeCount: mediumSizeCount ?? this.mediumSizeCount,
+      largeSizeCount: largeSizeCount ?? this.largeSizeCount,
+    );
+  }
+
+// JSON serialization methods go here (if needed)
+}
