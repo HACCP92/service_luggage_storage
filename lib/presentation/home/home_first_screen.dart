ea0f350198034769975419d28c3b0b8a2c978727
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-09-20 23:31:20 +0900

    [물품보관소 서비스 프로젝트] geolocator 적용해서 screen 수정

--- lib/presentation/home/home_first_screen.dart
+++ lib/presentation/home/home_first_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomeFirstScreen extends StatelessWidget {
+class HomeFirstScreen extends StatefulWidget {
   const HomeFirstScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeFirstScreenState createState() => _HomeFirstScreenState();
+}
+
+class _HomeFirstScreenState extends State<HomeFirstScreen> {
+  NMapController? _controller;
+  NMarker? _currentLocationMarker;
+
+  @override
   Widget build(BuildContext context) {
-    final marker = NMarker(
-        id: 'test',
-        icon: NOverlayImage.fromAssetImage('assets/marker_icon.png'),
-        position: NLatLng(37.476839, 126.964602));
     return Scaffold(
       body: NaverMap(
         options: const NaverMapViewOptions(
@@ -17,16 +22,56 @@ class HomeFirstScreen extends StatelessWidget {
           locationButtonEnable: true,
           consumeSymbolTapEvents: false,
           initialCameraPosition: NCameraPosition(
-              target: NLatLng(37.476839, 126.964602),
-              zoom: 15,
-              bearing: 0,
-              tilt: 0),
+            target: NLatLng(37.476839, 126.964602),
+            zoom: 15,
+            bearing: 0,
+            tilt: 0,
+          ),
         ),
         onMapReady: (controller) async {
-          controller.addOverlay(marker);
-          // 지도 컨트롤러가 준비되었을 때 수행할 작업
+          setState(() {
+            _controller = controller as NMapController?;
+          });
+
+          // 현재 위치를 가져옵니다.
+          final Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+          );
+
+          // 현재 위치에 마커를 추가합니다.
+          _addCurrentLocationMarker(position);
         },
       ),
     );
   }
+
+  void _addCurrentLocationMarker(Position position) {
+    final NLatLng currentLatLng =
+        NLatLng(position.latitude, position.longitude);
+
+    // 기존 마커가 있다면 제거합니다.
+    if (_currentLocationMarker != null) {
+      _controller?.removeOverlay(_currentLocationMarker!);
+    }
+
+    // 새로운 현재 위치 마커를 생성합니다.
+    final NMarker currentLocationMarker = NMarker(
+      id: 'current_location',
+      icon: NOverlayImage.fromAssetImage('assets/marker_icon.png'),
+      position: currentLatLng,
+    );
+
+    // 지도에 마커를 추가합니다.
+    _controller?.addOverlay(currentLocationMarker);
+
+    setState(() {
+      _currentLocationMarker = currentLocationMarker;
+    });
+  }
+}
+
+class NMapController {
+  void addOverlay(NMarker currentLocationMarker) {}
+
+  void removeOverlay(NMarker nMarker) {}
 }
