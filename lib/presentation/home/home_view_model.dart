d6024c9f1dd865cbb9832e30cbe9a2c0d9aeda5a
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-10-05 23:10:41 +0900

    [물품보관소 서비스 프로젝트] MVVM패턴으로 HomeViewModel로 분리하여 작성

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -1,19 +1,44 @@
-import 'package:flutter/cupertino.dart';
-import 'package:service_luggage_storage/data/geolocator_location_tracker.dart';
-import 'package:service_luggage_storage/domain/location/location_tracker.dart';
-import 'package:service_luggage_storage/presentation/home/home_state.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-class HomeViewModel with ChangeNotifier {
-  final LocationTracker _locationTracker = GeolocatorLocationTracker();
+class HomeViewModel {
+  _NaverMapControllerImpl? _controller;
+  NMarker? _currentLocationMarker;
 
-  HomeState _state = HomeState(username: '천정환');
-  HomeState get state => _state;
+  Future<void> initMapController() async {
+    final Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
 
-  HomeViewModel() {
-    _locationTracker.getLocation().then((location) {
-      final newState = state.copyWith(currentLocation: location);
-      _state = newState;
-      notifyListeners();
-    });
+    _addCurrentLocationMarker(position);
   }
+
+  void setMapController(_NaverMapControllerImpl controller) {
+    _controller = controller;
+  }
+
+  void _addCurrentLocationMarker(Position position) {
+    final NLatLng currentLatLng =
+        NLatLng(position.latitude, position.longitude);
+
+    if (_currentLocationMarker != null) {
+      _controller?.removeOverlay(_currentLocationMarker!);
+    }
+
+    final NMarker currentLocationMarker = NMarker(
+      id: 'current_location',
+      icon: NOverlayImage.fromAssetImage('assets/marker_icon.png'),
+      position: currentLatLng,
+    );
+
+    _controller?.addOverlay(currentLocationMarker);
+
+    _currentLocationMarker = currentLocationMarker;
+  }
+}
+
+class _NaverMapControllerImpl {
+  void removeOverlay(NMarker nMarker) {}
+
+  void addOverlay(NMarker currentLocationMarker) {}
 }
